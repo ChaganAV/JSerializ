a0b668c6d53f24fbbb467f424229f69cf96c14b4
Alexandr <jwchaganav@gmail.com>
2023-12-11 14:36:16 +0400

    json

--- src/main/java/ru/geekbrains/lesson3/task3/Program.java
+++ src/main/java/ru/geekbrains/lesson3/task3/Program.java
@@ -1,24 +1,27 @@
 package ru.geekbrains.lesson3.task3;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Student student = new Student("Вася",25,4);
-        try(FileOutputStream fileOutputStream = new FileOutputStream("student.bin")){
-            try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)){
-                objectOutputStream.writeObject(student);
-                System.out.println("Объект student сериализовали");
-            }
-        }
-        Student studentNew;
-        try(FileInputStream fileInputStream = new FileInputStream("student.bin")){
-            try(ObjectInputStream objectInputStream = new ObjectInputStream((fileInputStream))){
-                studentNew = (Student) objectInputStream.readObject();
-                System.out.println("десериализуем");
-                System.out.println(studentNew);
-                System.out.println("GPA не сериализовался, потому что transient");
-            }
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Вася",25,4));
+        students.add(new Student("Маша",23,3));
+        students.add(new Student("Миша",24,2));
+        students.add(new Student("Вова",27,8));
+
+        StudentList.saveToFile("Students.bin",students);
+        StudentList.saveToFile("Students.json",students);
+        StudentList.saveToFile("Students.xml",students);
+        System.out.println("Сериализовали");
+        System.out.println("=============");
+
+        List<Student> studentsNew = StudentList.loadFromFile("students.json");
+        //studentsNew.forEach(s -> System.out.println(s));
+        for (Student student: studentsNew) {
+            System.out.println(student);
         }
     }
 }
---
+++ src/main/java/ru/geekbrains/lesson3/task3/StudentList.java
@@ -0,0 +1,68 @@
+package ru.geekbrains.lesson3.task3;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentList {
+    public static final String FILE_JSON = "students.json";
+    public static final String FILE_BIN = "students.bin";
+    public static final String FILE_XML = "students.xml";
+
+    // для JSON
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    // для XML
+    private static final XmlMapper xmlMapper = new XmlMapper();
+    public static void saveToFile(String filename, List<Student> students){
+        try{
+            if(filename.endsWith(".json")){
+                objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+                objectMapper.writeValue(new File(filename),students);
+            }else if(filename.endsWith(".bin")){
+                try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                        new FileOutputStream(filename))){
+                    objectOutputStream.writeObject(students);
+                }
+            }else if(filename.endsWith(".xml")){
+                xmlMapper.writeValue(new File(filename),students);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static List<Student> loadFromFile(String filename){
+        List<Student> students = new ArrayList<>();
+        File file = new File(filename);
+        if(file.exists()){
+            try{
+                if(filename.endsWith(".json")){
+                    students = objectMapper.readValue(file,
+                            objectMapper.getTypeFactory().constructCollectionType(List.class,Student.class));
+                }else if(filename.endsWith(".bin")){
+                    try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+                        students = (List<Student>) objectInputStream.readObject();
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else if(filename.endsWith(".xml")){
+                    students = xmlMapper.readValue(file,
+                            xmlMapper.getTypeFactory().constructCollectionType(List.class,Student.class));
+                }
+            } catch (StreamReadException e) {
+                throw new RuntimeException(e);
+            } catch (DatabindException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return students;
+    }
+}
